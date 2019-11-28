eab4b39f0d7e78b805926d6f07af2dbf20841dff
waerdeng33 <58284424+waerdeng33@users.noreply.github.com>
2019-11-28 18:45:04 +1100

    Add files via upload

---
+++ Coursera_Capstone_Notebook.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Coursera IBM Capstone Project
+
+# ## Week1
+
+# In[1]:
+
+
+import pandas as pd
+import numpy as np
+
+
+# In[2]:
+
+
+print("Hello Capstone Project Course!")
+
+
+# In[ ]:
+
+
+
+
